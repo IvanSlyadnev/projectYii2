3fdb027a7e768972b1a87afb23f3cbe611e6f6b7
IvanSlyadnev <ivanslyadnev2015@gmail.com>
2021-04-15 22:05:41 +0300

    'commit'

--- views/chat/index.php
+++ views/chat/index.php
@@ -35,39 +35,6 @@ use \yii\helpers\Html;
     ActiveForm::end();
     ?>
 </div>
-<?php
-$script = <<< JS
-    $(document).ready(function() {
-        setInterval(function(){ $("#refreshButton").click(); console.log("a");}, 1000);
-    });
-JS;
-$this->registerJs($script);
-?>
-    <?php Pjax::begin(); ?>
-    <?= Html::a(
-        'Обновить',
-        ['index'],
-        ['class' => 'btn btn-lg btn-primary', 'id' => 'refreshButton']
-    ) ?>
-
-<?php Pjax::end(); ?>
-<div id = "d"></div>
-<input type="text" name="title">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript">
-    var mes = document.getElementById("mes");
-    mes.scrollTop = mes.scrollHeight;
-        var d = document.getElementById("d");
-        setInterval(function () {
-            d.innerHTML = "";
-            d.innerHTML = <?php foreach ($messages as $message) :?>
-                <br>
-                <?=$message->text?>
-            <?endforeach;?>
-        }, 2000);
-
-
-</script>
 </body>
 </div>
 
